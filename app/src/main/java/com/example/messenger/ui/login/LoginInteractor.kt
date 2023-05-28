d1aa96c73c0d95e10ea82158d42e30d6b2656981
Manul228 <qawaii@yandex.ru>
2023-05-28 20:08:31 +0300

    loginInteractorImpl

---
+++ app/src/main/java/com/example/messenger/ui/login/LoginInteractor.kt
@@ -0,0 +1,14 @@
+package com.example.messenger.ui.login
+
+import com.example.messenger.data.local.AppPreferences
+import com.example.messenger.ui.auth.AuthInteractor
+
+interface LoginInteractor : AuthInteractor {
+    interface OnDetailsRetrievalFinishedListener {
+        fun onDetailsRetrievalSuccess()
+        fun onDetailsRetrievalError()
+    }
+
+    fun login(username: String, listener: AuthInteractor.onAuthFinishedListener)
+    fun retrieveDetails(preferences: AppPreferences, listener: OnDetailsRetrievalFinishedListener)
+}
---
+++ app/src/main/java/com/example/messenger/ui/login/LoginInteractorImpl.kt
@@ -0,0 +1,51 @@
+package com.example.messenger.ui.login
+
+import com.example.messenger.data.local.AppPreferences
+import com.example.messenger.data.remote.request.LoginRequestObject
+import com.example.messenger.service.MessengerApiService
+import com.example.messenger.ui.auth.AuthInteractor
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class LoginInteractorImpl : LoginInteractor {
+    override lateinit var accessToken: String
+    override lateinit var submittedUsername: String
+    override lateinit var submittedPassword: String
+
+    private val service: MessengerApiService = MessengerApiService.getInstance()
+
+    override fun login(username: String, password: String, listener: AuthInteractor.onAuthFinishedListener) {
+        when {
+            username.isBlank() -> listener.onUsernameError()
+            password.isBlank() -> listener.onPasswordError()
+            else -> {
+                submittedUsername = username
+                submittedPassword = password
+                val requestObject = LoginRequestObject(username, password)
+
+                service.login(requestObject)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({ res ->
+                        if (res.code() != 403) {
+                            accessToken = res.headers()["Authorization"] as String
+                            listener.onAuthSuccess()
+                        } else {
+                            listener.onAuthError()
+                        }
+                    }, {error ->
+                        listener.onAuthError()
+                        error.printStackTrace()
+                })
+            }
+        }
+    }
+
+    override fun persistAccessToken(preferences: AppPreferences) {
+        preferences.storeAccessToken(accessToken)
+    }
+
+    override fun persistUserDetails(preferences: AppPreferences) {
+        preferences.storeUserDetails(userDetails)
+    }
+}
